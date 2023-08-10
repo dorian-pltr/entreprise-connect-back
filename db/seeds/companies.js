81e5b4ca017a2bb9245fa989226b82fb3f78e945
urbadorian <pelletier.dorian@urbasolar.com>
2023-08-10 15:05:56 +0200

    reformat: prettier

--- db/seeds/companies.js
+++ db/seeds/companies.js
@@ -1,8 +1,10 @@
 exports.seed = function (knex) {
-    return knex('companies').del().then(function () {
-        return knex('companies').insert([
-            { name: 'Entreprise 1', address: 'Adresse 1' },
-            { name: 'Entreprise 2', address: 'Adresse 2' },
-        ]);
-    });
-};
+  return knex('companies')
+    .del()
+    .then(function () {
+      return knex('companies').insert([
+        { name: 'Entreprise 1', address: 'Adresse 1' },
+        { name: 'Entreprise 2', address: 'Adresse 2' },
+      ])
+    })
+}
--- knexfile.js
+++ knexfile.js
@@ -1,21 +1,21 @@
-require('dotenv').config(); // Charge les variables d'environnement depuis le fichier .env
+require('dotenv').config() // Charge les variables d'environnement depuis le fichier .env
 
 module.exports = {
-    development: {
-        client: 'pg', // Utilise PostgreSQL
-        connection: {
-            host: process.env.DB_HOST,
-            port: process.env.DB_PORT,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME,
-        },
-        migrations: {
-            directory: './db/migrations',
-        },
-        seeds: {
-            directory: './db/seeds',
-        },
-        useNullAsDefault: true,
+  development: {
+    client: 'pg', // Utilise PostgreSQL
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
     },
-};
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds',
+    },
+    useNullAsDefault: true,
+  },
+}
